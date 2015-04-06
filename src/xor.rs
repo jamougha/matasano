7e8c31978148f865b70a5bbdb44b9752773c56c1
James Moughan <jamougha@gmail.com>
2015-04-06 21:05:53 +0100

    Solved challenge 4, set 1

--- src/xor.rs
+++ src/xor.rs
@@ -1,5 +1,34 @@
+use std::iter::repeat;
+
 pub fn encode<I, J>(a: I, b: J) -> Vec<u8>
     where I: Iterator<Item = u8>, J: Iterator<Item = u8>
 {
     a.zip(b).map(|(ab, bb)| ab ^ bb).collect()
+}
+
+pub fn best_decoding(ciphertext: &[u8]) -> (u32, String) {
+
+    let mut best_key = 0u8;
+    let mut best_key_score = 0u32;
+
+    for key_byte in 0.. {
+        let mut counts = [0u32; 256];
+        let key = repeat(key_byte);
+        let plaintext = encode(key, ciphertext.iter().map(|b| *b));
+        for b in plaintext {
+            counts[b as usize] += 1;
+        }
+
+        let score = "etaoin shrudlu".chars().fold(0, |score, c| score + counts[c as usize]);
+        if score > best_key_score {
+            best_key = key_byte;
+            best_key_score = score;
+        }
+
+    }
+
+    let plaintext = encode(repeat(best_key), ciphertext.iter().map(|b| *b));
+
+    (best_key_score, plaintext.iter().map(|x| *x as char).collect::<String>())
+
 }
